3180bea66b342f55d2787cd961fcccb0064da9e5
illusi0n <miloshb92@hotmail.com>
2019-02-20 23:42:20 +0100

    Added saving orders in dynamodb

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,13 +1,29 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 const createOrder = order => {
-  console.log(`order=${JSON.stringify(order)}`);
 
-  if (!order || !order.pizzaId || !order.address) {
+  if (!order || !order.pizza || !order.address) {
     throw new Error('Missing parameters for creating new order');
   }
 
-  return {
-  };
+  return docClient.put({
+    TableName: 'pizza-orders',
+    Item: {
+      orderId: 'some-id',
+      pizza: order.pizza,
+      address: order.address,
+      orderStatus: 'pending',
+    }
+  }).promise()
+    .then( res => {
+      console.log('Order is saved!', res);
+      return res;
+    })
+    .catch(saveError => {
+      console.log('Oops, order is not saved ', saveError);
+      throw saveError;
+    });
 }
 
 module.exports = createOrder;
